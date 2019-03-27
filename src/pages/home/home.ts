61320ac157060419ff2fe020fa2ea9d1ef7f2f9b
Your Name <you@example.com>
2019-03-27 14:38:36 -0600

    Se agregaron mas datos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { InfoPage } from '../info/info';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
   casas = [];
+  infoPage = InfoPage;
+
+
   constructor(public navCtrl: NavController, 
     public http: HttpClient) {
 
@@ -20,5 +24,8 @@ export class HomePage {
       });
 
   }
-
+  info(casa) {
+    console.log('click');
+    this.navCtrl.push(this.infoPage,{casa: casa});
+}
 }
